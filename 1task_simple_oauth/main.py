a8b0b8d849ef26c84f5585af45ae7ee5ef581f5f
yffi4 <ikulesh200515@mail.ru>
2025-12-13 12:35:33 +0500

    dockerfile changes1

--- 1task_simple_oauth/main.py
+++ 1task_simple_oauth/main.py
@@ -1,6 +1,20 @@
 from fastapi import FastAPI
 from auth.router import router as auth_router
+import uvicorn
+from dotenv import load_dotenv
+import os
 
+load_dotenv()
 app = FastAPI()
+
 app.include_router(auth_router)
 
+if __name__ == "__main__":
+    port = int(os.getenv("PORT", 8000))  # Nomad -> PORT
+
+    uvicorn.run(
+        "main:app",
+        host="0.0.0.0",
+        port=port,
+        log_level="info",
+    )
